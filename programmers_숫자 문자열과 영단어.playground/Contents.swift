3281188b417bff659d4de1b4e056e79bea79351c
윤희철 <kkll135@naver.com>
2022-01-13 00:04:41 +0900

    PS

--- programmers_숫자 문자열과 영단어.playground/Contents.swift
+++ programmers_숫자 문자열과 영단어.playground/Contents.swift
@@ -6,26 +6,34 @@ func solution(_ s: String) -> Int {
     
     if arr.contains("one") {
         arr = arr.replacingOccurrences(of: "one", with: "1")
-    } else if arr.contains("two") {
+    }
+    if arr.contains("two") {
         arr = arr.replacingOccurrences(of: "two", with: "2")
-    } else if arr.contains("three") {
+    }
+    if arr.contains("three") {
         arr = arr.replacingOccurrences(of: "three", with: "3")
-    } else if arr.contains("four") {
+    }
+    if arr.contains("four") {
         arr = arr.replacingOccurrences(of: "four", with: "4")
-    } else if arr.contains("five") {
+    }
+    if arr.contains("five") {
         arr = arr.replacingOccurrences(of: "five", with: "5")
-    } else if arr.contains("six") {
+    }
+    if arr.contains("six") {
         arr = arr.replacingOccurrences(of: "six", with: "6")
-    } else if arr.contains("seven") {
+    }
+    if arr.contains("seven") {
         arr = arr.replacingOccurrences(of: "seven", with: "7")
-    } else if arr.contains("eight") {
+    }
+    if arr.contains("eight") {
         arr = arr.replacingOccurrences(of: "eight", with: "8")
-    } else if arr.contains("nine") {
+    }
+    if arr.contains("nine") {
         arr = arr.replacingOccurrences(of: "nine", with: "9")
-    } else if arr.contains("zero") {
+    }
+    if arr.contains("zero") {
         arr = arr.replacingOccurrences(of: "zero", with: "0")
     }
     return Int(arr)!
 }
-
-solution("one2three4")
+solution("one4sevennine8")
