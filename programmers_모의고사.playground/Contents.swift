61c674ebc50b2f63749ed162ee9c0e3b16eb6c2f
윤희철 <kkll135@naver.com>
2022-01-13 19:52:40 +0900

    PS

---
+++ programmers_모의고사.playground/Contents.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+func solution(_ answers:[Int]) -> [Int] {
+    
+    let first: [Int] = [1,2,3,4,5]
+    let second: [Int] = [2,1,2,3,2,4,2,5]
+    let third: [Int] = [3,3,1,1,2,2,4,4,5,5]
+    
+    var arr1: [Int] = []
+    var arr2: [Int] = []
+    var arr3: [Int] = []
+    
+    var count: [Int] = [0,0,0]
+    
+    for i in 0..<answers.count {
+        arr1.append(first[(i % first.count)])
+        arr2.append(second[(i % second.count)])
+        arr3.append(third[(i % third.count)])
+        
+        if answers[i] == arr1[i] {
+            count[0] += 1
+        }
+        if answers[i] == arr2[i] {
+            count[1] += 1
+        }
+        if answers[i] == arr3[i] {
+            count[2] += 1
+        }
+    }
+    
+    count.sorted {$0 > $1}
+    print(count.indices)
+    return count
+}
+solution([1,1,2,1,5])
