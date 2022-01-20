bbddc3f1cb4fd677d6b9cac5c997805cc88b8da7
윤희철 <kkll135@naver.com>
2022-01-21 06:35:19 +0900

    PS

--- programmers_문자열 압축.playground/Contents.swift
+++ programmers_문자열 압축.playground/Contents.swift
@@ -1,34 +1,30 @@
 import Foundation
 
 func solution(_ s: String) -> Int {
-    
-    var s = s
-    var count: Int
-    var index: String.Index
+    // 문자열 단위로 나눠주기 -> 압축하기
     var str: String = ""
     var arr: [String] = []
     
-    if s.count == 1 {   //길이가 1일 경우
-        return 1
+    
+    for i in 1...s.count/2 {
+        var s = s
+        var str = ""
+        //단위에 따라 문자열 자르고 배열에 추가
+        for j in s{
+            str.append(j)
+            s.removeFirst()
+            if s.isEmpty {
+                arr.append(str)
+                continue
+            }
+            if str.count == i {
+                arr.append(str)
+                str = ""
+            }
+        }
+        print(arr)
+        arr = []
     }
-    //일단 문자열 1개 단위로 자르는 경우만 해보기 (i = 1인 경우)
-    index = s.index(s.startIndex, offsetBy: 0)
-    str.append(contentsOf: s[s.startIndex...index])
-    arr.append(str)
-    s.removeSubrange(s.startIndex...index)
-    str.append(contentsOf: s[s.startIndex...index])
-    arr.append(str)
-    print(arr)
-    
-    
-    
     return 0
 }
-solution("xababcdcdababcdcd")
-//
-//let a = "123456789"
-//for i in a {
-//    print(i)
-//    print(i-1)
-//}
-
+solution("xaavkdsjviajidjmcioaa")
