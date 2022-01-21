760d7455e77dab8814dd6a12536d8b338eb4c56e
윤희철 <kkll135@naver.com>
2022-01-21 23:47:18 +0900

    PS

--- programmers_문자열 압축.playground/Contents.swift
+++ programmers_문자열 압축.playground/Contents.swift
@@ -2,10 +2,12 @@ import Foundation
 
 func solution(_ s: String) -> Int {
     // 문자열 단위로 나눠주기 -> 압축하기
-    var str: String = ""
     var arr: [String] = []
-    
-    
+    var result: String = ""
+    var resultCount: [Int] = []
+    var count: Int = 1
+    var length: Int
+
     for i in 1...s.count/2 {
         var s = s
         var str = ""
@@ -23,8 +25,33 @@ func solution(_ s: String) -> Int {
             }
         }
         print(arr)
+        length = arr.count
+        //
+        //문자열을 압축
+        for j in 0..<length {
+            count = 1
+            for k in j+1..<length {
+                if arr[j] == arr[k] {
+                    count += 1
+                    continue
+                }
+                if arr[j] != arr[k] {
+                    if count != 1 {
+                        result += "\(count)\(arr[j])"
+                        
+                        break
+                    } else {
+                        result += arr[j]
+                        
+                    }
+                }
+            }
+        }
+        resultCount.append(result.count)
+        print(resultCount)
         arr = []
     }
     return 0
 }
-solution("xaavkdsjviajidjmcioaa")
+solution("aabbabcc")
+
