1d804cb7335d47fbdc9ce63a8e5b4614269e9d16
윤희철 <kkll135@naver.com>
2022-01-30 04:44:47 +0900

    PS

--- BOJ_크로아티아 알파벳.playground/Contents.swift
+++ BOJ_크로아티아 알파벳.playground/Contents.swift
@@ -1,4 +1,4 @@
-let word: String = "dzc="
+let word: String = "dz"
 var count: Int = 0
 var temp: String = ""
 var arr: [String] = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=" ]
@@ -12,22 +12,51 @@ for char in word {
         }
     } else {
         temp.append(char) // count가 2인 상태
-        if arr.contains(temp) {
-            count += 1
-            temp = ""
-        } else {
-            if temp == "dz" {
-                continue
+        if temp.first != "d" {
+            if arr.contains(temp) {
+                count += 1
+                temp = ""
             } else {
-                if temp.count == 2 {
+                if char == "c" || char == "d" || char == "l" || char == "n" || char == "s" || char == "z"  {
                     count += 1
                     temp.removeFirst()
-                } else{
-                    count += 3
+                } else {
+                    count += 2
                     temp = ""
+                }
+            }
+        } else { //temp의 맨 앞이 d인 경우
+            if arr.contains(temp) { //dz=, d-인 경우
+                count += 1
+                temp = ""
+            } else {
+                if temp == "dz" {
+                    continue
+                } else {
+                    if temp.count == 3 { //dz? 인 경우
+                        if char == "c" || char == "d" || char == "l" || char == "n" || char == "s" || char == "z"  {
+                            count += 2
+                            temp.removeFirst()
+                            temp.removeFirst()
+                        } else {
+                            count += 3
+                            temp = ""
+                        }
+                    } else { // d? 인 경우
+                        if char == "c" || char == "d" || char == "l" || char == "n" || char == "s" || char == "z"  {
+                            count += 1
+                            temp.removeFirst()
+                        } else {
+                            count += 2
+                            temp = ""
+                        }
+                    }
                 }
             }
         }
     }
+}
+if !temp.isEmpty {
+    count += temp.count
 }
 print(count)
