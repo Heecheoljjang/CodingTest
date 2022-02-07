1b90887ded565a54fc409ee844f6bc59b6e95929
윤희철 <kkll135@naver.com>
2022-02-07 22:43:43 +0900

    PS

--- BOJ_괄호.playground/Contents.swift
+++ BOJ_괄호.playground/Contents.swift
@@ -1,35 +1,29 @@
 import Foundation
 
-//let T = Int(readLine()!)!
-//var str = [String](repeating: "", count: T)
-//
-//for i in 0..<T {
-//    str[i] = readLine()!
-//}
+let T = Int(readLine()!)!
+var str = [String](repeating: "", count: T)
 
-let T = 6
-let str = ["(())())", "(((()())()", "(()())((()))", "((()()(()))(((())))()", "(()((())()("]
+for i in 0..<T {
+    str[i] = readLine()!
+}
 
 for ps in str {
     var left = 0
     var right = 0
-    if ps.first == ")" {
-        print("NO")
-        continue
-    } else if ps.last == "(" {
-        print("NO")
-        continue
-    } else {
-        for element in ps {
-            if element == "(" {
-                left += 1
-            } else {
-                right += 1
-                if right > left {
-                    print("NO")
-                    break
-                }
+    
+    for element in ps {
+        if element == "(" {
+            left += 1
+        } else {
+            right += 1
+            if right > left {
+                break
             }
         }
+    }
+    if left == right {
+        print("YES")
+    } else {
+        print("NO")
     }
 }
