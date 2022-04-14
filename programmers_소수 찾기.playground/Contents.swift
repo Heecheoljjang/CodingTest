03c975242385d6e255eebd60e672248c5f40f572
Heecheoljjang <kkll135@naver.com>
2022-04-14 16:09:04 +0900

    PS

--- programmers_소수 찾기.playground/Contents.swift
+++ programmers_소수 찾기.playground/Contents.swift
@@ -3,19 +3,27 @@ import Foundation
 func solution(_ numbers:String) -> Int {
     
     var arr = [Int](repeating: 0, count: 10000000) // 7자리의 가장 큰 수 9999999까지
-    var numArr: String = ""
-    var numCount: Int = 1
+    var numArr = numbers.map {String($0)} //배열로 바꿈
+    var numCheck = [Int](repeating: 0, count: numArr.count)
+    var number = ""
     
-    // 숫자리를 한자리씩 numArr에 넣음
-    for i in numbers {
-        if !numArr.contains(i) {
-            numArr.append(i)
+    // 숫자 만드는 함수
+    func DFS(_ depth: Int, _ numStr: String, _ count: Int) {
+        if depth == count { // 모든 숫자 완성
+            
+        } else { // 숫자만들기
+            for i in 0..<numArr.count {
+                if numCheck[i] == 0 {
+                    number += numArr[i]
+                    numCheck[i] = 1
+                    DFS(depth + 1, number, count)
+                }
+            }
         }
     }
-    // 만들 수 있는 수
-    while numCount != numArr.count {
-        
-    }
+    
+    
+    
     
 
     return 0
