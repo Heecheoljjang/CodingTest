1f547905e1fa15ade217f6596998587bb1c88581
Heecheoljjang <kkll135@naver.com>
2022-04-15 03:08:00 +0900

    PS

--- programmers_소수 찾기.playground/Contents.swift
+++ programmers_소수 찾기.playground/Contents.swift
@@ -1,24 +1,44 @@
 import Foundation
 
 func solution(_ numbers:String) -> Int {
-    
-    var arr = [Int](repeating: 0, count: 10000000) // 7자리의 가장 큰 수 9999999까지
+
     let numArr = numbers.map {String($0)} //배열로 바꿈 -> map 정리
+    var strNumArr: [String] = []
     var numCheck = [Int](repeating: 0, count: numArr.count)
     var number = ""
-    
+    var primeBool = true
+    var result: [Int] = []
+
+    for i in numArr {
+        strNumArr.append(i)
+    }
+    strNumArr = strNumArr.sorted {$0 < $1}
     // 소수확인 함수
     func isPrime(_ number: Int) {
-        if number > 1 {
-            
+        if number == 2 || number == 3 || number == 5 {
+            primeBool = true
+        } else if number == 0 || number == 1 {
+            primeBool = false
+        } else {
+            for i in 2...Int(sqrt(Double(number))) {
+                if number % i == 0 {
+                    primeBool = false
+                    break
+                }
+            }
         }
+
     }
-    
+
     // 숫자 만드는 함수
     func DFS(_ depth: Int, _ numStr: String, _ count: Int) { // -> DFS 알고리즘 정리
         if depth == count { // 숫자 완성(string형태)
             if let number = Int(number) {
-                arr[number] = 1
+                isPrime(number)
+                if primeBool == true && !result.contains(number) {
+                    result.append(number)
+                }
+                primeBool = true
             }
         } else { // 숫자만들기
             for i in 0..<numArr.count {
@@ -27,26 +47,14 @@ func solution(_ numbers:String) -> Int {
                     numCheck[i] = 1
                     DFS(depth + 1, number, count)
                     numCheck[i] = 0
-                    number = ""
+                    number = numStr
                 }
             }
         }
     }
-    for i in 1...numArr.count {
-        DFS(0, number, i)
+    for i in 1...strNumArr.count {
+        DFS(0, "", i)
     }
-    // 소수 판별
-    arr[0] = 0
-    arr[1] = 0
-
-    for i in 2...(arr.count - 1) {
-        for j in stride(from: i + i, through: arr.count, by: i) {
-            if arr.contains(j) {
-                arr[j] = 0
-            }
-        }
-    }
-    print("asdf: \(arr.filter{$0 == 1}.count)")
-    return arr.filter{$0 == 1}.count
+    return result.count
 }
-solution("1231323")
+
