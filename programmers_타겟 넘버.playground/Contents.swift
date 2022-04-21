b41bb3847cc2d49a77e0737dbb0ecfce724b72d5
Heecheoljjang <kkll135@naver.com>
2022-04-22 02:40:57 +0900

    PS

--- programmers_타겟 넘버.playground/Contents.swift
+++ programmers_타겟 넘버.playground/Contents.swift
@@ -2,10 +2,22 @@ import Foundation
 
 func solution(_ numbers:[Int], _ target:Int) -> Int {
     
-    func dfs(_ numbers: [Int], _ depth: Int, _ count: Int) -> Int {
-        
-        return 0 
+    var count: Int = 0
+
+    func dfs(_ index: Int, _ sum: Int) {
+        if index <= numbers.count {
+            if index == numbers.count - 1 {
+                if sum == target {
+                    count += 1
+                }
+                return
+            }
+            dfs(index + 1, sum + numbers[index + 1])
+            dfs(index + 1, sum - numbers[index + 1])
+        }
     }
-    
-    return 0
+    dfs(-1, 0)
+    return count
 }
+
+solution([4,1,2,1], 4)
