99b902d8a70f77fa9110014f00d2480e07521207
Heecheoljjang <kkll135@naver.com>
2022-04-21 15:14:26 +0900

    PS

---
+++ programmers_타겟 넘버.playground/Contents.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ numbers:[Int], _ target:Int) -> Int {
+    
+    func dfs(_ numbers: [Int], _ depth: Int, _ count: Int) -> Int {
+        
+        return 0 
+    }
+    
+    return 0
+}
