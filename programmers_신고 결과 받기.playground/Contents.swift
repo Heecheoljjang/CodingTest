181ac2d99028ee332715db98b1319f6715083da6
Heecheoljjang <kkll135@naver.com>
2022-04-25 04:26:40 +0900

    PS

--- programmers_신고 결과 받기.playground/Contents.swift
+++ programmers_신고 결과 받기.playground/Contents.swift
@@ -2,46 +2,36 @@ import Foundation
 
 func solution(_ id_list:[String], _ report:[String], _ k:Int) -> [Int] {
     
-    var reported: [String] = []
-    var temp: [String: Int] = [:]
-    var result: [String: Int] = [:]
-    var resultCount: [Int] = []
+    let report = Array(Set(report)) // 중복제거
+    var idIndex: [String: Int] = [:]
+    var resultCount = [Int](repeating: 0, count: id_list.count)
+    var reported: [String: [String]] = [:]
     
-    for i in report {
-        reported.append(i.components(separatedBy: " ")[1])
+    // id_list의 인덱스 -> for문으로 id_list 돌리면 시간 낭비
+    for (i, id) in id_list.enumerated() {
+        idIndex[id] = i
     }
-    
-    for i in reported {
-        if temp.keys.contains(i) {
-            temp[i]! += 1
+    // 신고 당한 횟수 구하기
+    for i in report {
+        let reportId = i.components(separatedBy: " ")[0]
+        let reportedId = i.components(separatedBy: " ")[1]
+        if reported[reportedId] == nil {
+            reported[reportedId] = [reportId]
         } else {
-            temp.updateValue(1, forKey: i)
+            reported[reportedId]?.append(reportId)
         }
     }
-    reported = []
-    for i in temp {
-        if i.value >= k {
-            reported.append(i.key)
-        }
-    }
-    print(reported)
-    for i in report {
-        if reported.contains(i.components(separatedBy: " ")[1]) {
-            if result.keys.contains(i.components(separatedBy: " ")[0]) {
-                result[i.components(separatedBy: " ")[0]]! += 1
-            } else {
-                result.updateValue(1, forKey: i.components(separatedBy: " ")[0])
+    for i in reported {
+        if i.value.count >= k {
+            for j in i.value {
+                resultCount[idIndex[j]!] += 1
             }
         }
     }
-    for i in id_list {
-        if result.keys.contains(i) {
-            resultCount.append(result[i]!)
-        } else {
-            resultCount.append(0)
-        }
-    }
-    
     return resultCount
 }
+
 solution(["muzi", "frodo", "apeach", "neo"], ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"], 2)
+
+
+//enumerate정리
