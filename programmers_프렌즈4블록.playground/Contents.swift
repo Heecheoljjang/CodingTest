bcfe3b43266d6c219431c0455d8f83c579e1f18c
Heecheoljjang <kkll135@naver.com>
2022-05-15 01:01:26 +0900

    PS

--- programmers_프렌즈4블록.playground/Contents.swift
+++ programmers_프렌즈4블록.playground/Contents.swift
@@ -4,6 +4,8 @@ func solution(_ m:Int, _ n:Int, _ board:[String]) -> Int {
 
     var block: [[String.Element]] = []
     var count: Int = 0
+    // while문을 끝내기 위해
+    var checkSame: Int = 0
     // 2차원 배열로 만들어줌
     for i in board {
         block.append(Array(i))
@@ -12,9 +14,9 @@ func solution(_ m:Int, _ n:Int, _ board:[String]) -> Int {
     // 탐색해서 지우는 함수와 배열하는 함수 구현
     
     // 탐색하는 함수
-    func check(_ arr: [[String.Element]], _ m: Int, _ n: Int) -> [[String.Element]] {
+    func check(_ arr: [[String.Element]], _ m: Int, _ n: Int) -> [[String.Element]]{
         
-        var delete: [String] = []
+        var delete: [[Int]] = []
         var newArr: [[String.Element]] = arr
         
         // m 높이, n 폭인데 마지막 칸의 오른쪽과 아래를 확인하면 범위를 벗어나므로
@@ -25,15 +27,11 @@ func solution(_ m:Int, _ n:Int, _ board:[String]) -> Int {
                     let temp = newArr[i][j]
                     // 네 칸이 다 같은 경우
                     if newArr[i][j+1] == temp && newArr[i+1][j] == temp && newArr[i+1][j+1] == temp {
-                        delete.append("arr[\(i)][\(j)]")
-                        delete.append("arr[\(i+1)][\(j)]")
-                        delete.append("arr[\(i)][\(j+1)]")
-                        delete.append("arr[\(i+1)][\(j+1)]")
-                        // 지운 부분은 "X"로
-                        newArr[i][j] = "X"
-                        newArr[i+1][j] = "X"
-                        newArr[i][j+1] = "X"
-                        newArr[i+1][j+1] = "X"
+                        delete.append([i,j])
+                        delete.append([i+1,j])
+                        delete.append([i,j+1])
+                        delete.append([i+1,j+1])
+                        
                     } else {
                         continue
                     }
@@ -42,35 +40,44 @@ func solution(_ m:Int, _ n:Int, _ board:[String]) -> Int {
         }
         // delete에서 중복되는 것을 지워준 뒤 count 계산
         count += Set(delete).count
-        
+        // 지운 부분은 "X"로
+        for i in delete {
+            newArr[i[0]][i[1]] = "X"
+        }
+        if newArr == arr {
+            checkSame = 1
+        }
         return newArr
     }
     // 새로 배열하는 함수
-    func makeNewArr(_ arr: [[String.Element]], _ m: Int, _ n: Int) -> [[String.Element]]{
+    func makeNewArr(_ arr: [[String.Element]], _ m: Int, _ n: Int) -> [[String.Element]] {
         
         var newArr: [[String.Element]] = arr
-        var checkN: Int = 0
+        var tempNum: Int = 0
         
-        // 새로로 한 줄씩 확인하면서 X인 부분이 있으면 아래로 내려줌
-        while checkN < n+1 {
-            for i in 0..<m-1 {
-                if newArr[checkN][i] != "X" {
-                    if newArr[checkN][i+1] == "X" {
-                        // 위에를 X로 만들고 아래의 X값을 바꿔줌
-                        newArr[checkN][i+1] == newArr[checkN][i]
-                        newArr[checkN][i] == "X"
+        // 세로로 한 줄씩 확인. 이때 아래에서부터 확인해야 더 간단함.
+        for i in 0..<n {
+            for j in (1..<m).reversed() {
+                if newArr[j][i] == "X" {
+                    tempNum = j
+                    while tempNum >= 0 {
+                        if newArr[tempNum][i] != "X" {
+                            newArr[j][i] = newArr[tempNum][i]
+                            newArr[tempNum][i] = "X"
+                            break
+                        } else {
+                            tempNum -= 1
+                        }
                     }
                 }
             }
-            
-            checkN += 1
         }
-        
-        
         return newArr
     }
-    
-    
+    while checkSame != 1 {
+        block = check(block, m, n)
+        block = makeNewArr(block, m, n)
+    }
     return count
 }
 solution(4, 5, ["CCBDE", "AAADE", "AAABF", "CCBBF"])
