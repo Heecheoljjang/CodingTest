9ba0d0fe7e9b9800fc870566eeaec2c89e873f83
hee <kkll135@naver.com>
2022-12-23 01:06:27 +0900

    PS

--- programmers_JadenCase 문자열 만들기.playground/Contents.swift
+++ programmers_JadenCase 문자열 만들기.playground/Contents.swift
@@ -2,24 +2,42 @@ import Foundation
 
 func solution(_ s:String) -> String {
     
-    let tempArr = s.split(separator: " ")
-    var result = ""
-    
-    for word in tempArr {
-        if word.first!.isLetter {
-            var temp = word.lowercased()
-            var first = "\(word.first!.uppercased())"
-            temp.removeFirst()
-            first += temp
-            result += first
-        } else {
-            result += word.lowercased()
+    var result: [String] = []
+    let wordArray = s.lowercased().components(separatedBy: " ")
+    for i in 0..<wordArray.count {
+        if wordArray[i].isEmpty {
+            result.append("")
+            continue
         }
-        result += " "
+        var temp = wordArray[i].map { String($0) }
+        temp[0] = temp[0].uppercased()
+        result.append(temp.joined())
     }
-    result.removeLast()
-    
-    return result
+    return result.joined(separator: " ")
 }
 
-solution("3people unFollowed me")
+//import Foundation
+//
+//func solution(_ s:String) -> String {
+//
+//    let tempArr = s.split(separator: " ")
+//    var result = ""
+//
+//    for word in tempArr {
+//        if word.first!.isLetter {
+//            var temp = word.lowercased()
+//            var first = "\(word.first!.uppercased())"
+//            temp.removeFirst()
+//            first += temp
+//            result += first
+//        } else {
+//            result += word.lowercased()
+//        }
+//        result += " "
+//    }
+//    result.removeLast()
+//
+//    return result
+//}
+//
+//solution("3people unFollowed me")
