5569f9064f50970b57555aeb0e9801c8f7448b62
hee <kkll135@naver.com>
2023-01-01 15:44:01 +0900

    PS

--- BOJ_병합정렬1.playground/Contents.swift
+++ BOJ_병합정렬1.playground/Contents.swift
@@ -1,34 +1,56 @@
 import Foundation
 
-func mergeSort(_ array: [Int]) -> [Int] {
-    if array.count <= 1 { return array }
-    let center = array.count / 2
-    let left = Array(array[0..<center])
-    let right = Array(array[center..<array.count])
-    
-    func merge(_ left: [Int],_ right: [Int]) -> [Int] {
-        var left = left
-        var right = right
-        var result: [Int] = []
-        
-        while !left.isEmpty && !right.isEmpty {
-            if left[0] < right[0] {
-                result.append(left.removeFirst())
-            } else {
-                result.append(right.removeFirst())
-            }
-        }
-        if !left.isEmpty {
-            result.append(contentsOf: left)
-        }
-        if !right.isEmpty {
-            result.append(contentsOf: right)
-        }
-        
-        return result
+var count = 0
+var result = -1
+func mergeSort(_ arr: [Int], _ p: Int, _ r: Int) {
+    if p < r {
+        let q = (p + r) / 2
+        mergeSort(arr, p, q)
+        mergeSort(arr, q + 1, r)
+        merge(arr, p, q, r)
     }
+}
+func merge(_ arr: [Int], _ p: Int, _ q: Int, _ r: Int) {
+    var i = p
+    var j = q + 1
+    var t = 1
+    var temp: [Int] = []
+    var newArr = arr
     
-    return merge(mergeSort(left), mergeSort(right))
+    while(i <= q && j <= r) {
+        if arr[i] <= arr[j] {
+            temp.append(arr[i])
+            i += 1
+        } else {
+            temp.append(arr[j])
+            j += 1
+        }
+    }
+    while i <= q {
+        temp.append(arr[i])
+        i += 1
+    }
+    while j <= r {
+        temp.append(arr[j])
+        j += 1
+    }
+    i = p
+    t = 0
+    while i <= r {
+        newArr[i] = temp[t]
+        count += 1
+        if count == k {
+            result = newArr[i]
+            break
+        }
+        i += 1
+        t += 1
+    }
 }
 
-let 
+let input = readLine()!.components(separatedBy: " ").map { Int(String($0))! }
+let arr = readLine()!.components(separatedBy: " ").map { Int(String($0))! }
+let n = input[0]
+let k = input[1]
+mergeSort(arr, 0, n-1)
+print(result)
