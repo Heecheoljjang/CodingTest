7999fb5bce1f46872f54ba4176c2e70f22a567a0
hee <kkll135@naver.com>
2023-01-03 14:18:47 +0900

    PS

--- BOJ_괄호.playground/Contents.swift
+++ BOJ_괄호.playground/Contents.swift
@@ -1,29 +1,51 @@
 import Foundation
 
-let T = Int(readLine()!)!
-var str = [String](repeating: "", count: T)
-
-for i in 0..<T {
-    str[i] = readLine()!
-}
-
-for ps in str {
+let t = Int(readLine()!)!
+var result = Array(repeating: "YES", count: t)
+for i in 0..<t {
+    var input = readLine()!.map { String($0) }
     var left = 0
     var right = 0
-    
-    for element in ps {
-        if element == "(" {
-            left += 1
-        } else {
+    while !input.isEmpty {
+        if input.removeLast() == ")" {
             right += 1
-            if right > left {
-                break
-            }
+        } else {
+            left += 1
+        }
+        if left > right {
+            result[i] = "NO"
+            break
         }
     }
-    if left == right {
-        print("YES")
-    } else {
-        print("NO")
+    if right != left {
+        result[i] = "NO"
     }
 }
+print(result)
+//let T = Int(readLine()!)!
+//var str = [String](repeating: "", count: T)
+//
+//for i in 0..<T {
+//    str[i] = readLine()!
+//}
+//
+//for ps in str {
+//    var left = 0
+//    var right = 0
+//
+//    for element in ps {
+//        if element == "(" {
+//            left += 1
+//        } else {
+//            right += 1
+//            if right > left {
+//                break
+//            }
+//        }
+//    }
+//    if left == right {
+//        print("YES")
+//    } else {
+//        print("NO")
+//    }
+//}
