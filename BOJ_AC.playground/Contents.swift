65cbbd0895648d4347894cf055ad8b17345b51eb
hee <kkll135@naver.com>
2023-01-09 05:01:59 +0900

    PS

--- BOJ_AC.playground/Contents.swift
+++ BOJ_AC.playground/Contents.swift
@@ -1,45 +1,38 @@
-//import Foundation
-//
-////let caseCount = Int(readLine()!)!
-//let caseCount = 1
-//let input = [["DDDD", "2", "[1,2]"]]
-//outer: for i in 0..<caseCount {
-//    //    let command = readLine()!.map { $0 }
-//    //    let n = Int(readLine()!)!
-//    let command = input[i][0]
-//    let n = Int(input[i][1])!
-//    var numbers = input[i][2].dropFirst().dropLast().split(separator: ",").map { Int(String($0))! }
-//    var isR = false
-//    var head = 0
-//    var tail = numbers.count - 1
-//    for char in command {
-//        if char == "R" {
-//            isR.toggle()
-//        } else {
-//            if tail < head {
-//                print("error")
-//                continue outer
-//            }
-//            if isR {
-//                tail -= 1
-//            } else {
-//                head += 1
-//            }
-//        }
-//    }
-//    if head > tail {
-//        print("[]")
-//    } else {
-//        if isR {
-//            numbers = numbers[head...tail].reversed()
-//        } else {
-//            numbers = Array(numbers[head...tail])
-//        }
-//        print("[\(numbers.map { String($0) }.joined(separator: ","))]")
-//    }
-//}
-//
+import Foundation
 
-var a = [1,2,3]
-a = Array(a[1...2])
-print(a)
+let caseCount = Int(readLine()!)!
+outer: for _ in 0..<caseCount {
+    let command = readLine()!
+    let _ = Int(readLine()!)!
+    var numbers = readLine()!.dropFirst().dropLast().split(separator: ",")
+    
+    var isR = false
+    var head = 0
+    var tail = numbers.count - 1
+    
+    for char in command {
+        if char == "R" {
+            isR.toggle()
+        } else {
+            if tail < head {
+                print("error")
+                continue outer
+            }
+            if isR {
+                tail -= 1
+            } else {
+                head += 1
+            }
+        }
+    }
+    if head > tail {
+        print("[]")
+    } else {
+        if isR {
+            numbers = numbers[head...tail].reversed()
+        } else {
+            numbers = Array(numbers[head...tail])
+        }
+        print("[\(numbers.joined(separator: ","))]")
+    }
+}
