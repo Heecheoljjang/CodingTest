887eb6ffacec8ce975e1aad0c04fb05dc1dbbc20
hee <kkll135@naver.com>
2023-01-11 01:49:39 +0900

    수정

--- BOJ_미로탐색.playground/Contents.swift
+++ BOJ_미로탐색.playground/Contents.swift
@@ -1,17 +1,15 @@
 import Foundation
 
 let input = readLine()!.split(separator: " ").map { Int(String($0))! }
-var maze: [[String]] = []
+var maze: [[Int]] = []
 for _ in 0..<input[0] {
-    maze.append(readLine()!.map { String($0) })
+    maze.append(readLine()!.map { Int(String($0))! })
 }
 let dx = [1, 0, -1, 0]
 let dy = [0, 1, 0, -1]
 var left: [[Int]] = []
 var right: [[Int]] = []
 var count = 0
-var distance = Array(repeating: Array(repeating: -1, count: input[1]), count: input[0])
-distance[0][0] = 0
 left.append([0,0])
 while !left.isEmpty || !right.isEmpty {
     var current: [Int] = []
@@ -26,11 +24,10 @@ while !left.isEmpty || !right.isEmpty {
         if newX < 0 || newY < 0 || newX >= input[0] || newY >= input[1] {
             continue
         }
-        if distance[newX][newY] >= 0 || maze[newX][newY] != "1" {
-            continue
+        if maze[newX][newY] == 1 {
+            maze[newX][newY] = maze[current[0]][current[1]] + 1
+            left.append([newX,newY])
         }
-        distance[newX][newY] = distance[current[0]][current[1]] + 1
-        left.append([newX,newY])
     }
 }
-print(distance[input[0] - 1][input[1] - 1] + 1)
+print(maze[input[0] - 1][input[1] - 1])
