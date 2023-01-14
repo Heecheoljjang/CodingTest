966aee4ffe6464df8b5d97645fb7f14e18db6792
hee <kkll135@naver.com>
2023-01-14 23:04:16 +0900

    PS

--- BOJ_불.playground/Contents.swift
+++ BOJ_불.playground/Contents.swift
@@ -1,74 +1,76 @@
 import Foundation
 
-let input = readLine()!.split(separator: " ").map { Int(String($0))! }
-var maze: [[String.Element]] = []
-var fire = Array(repeating: Array(repeating: -1, count: input[1]), count: input[0])
-var person = Array(repeating: Array(repeating: -1, count: input[1]), count: input[0])
-var fireQueue: [[Int]] = []
-var personQueue: [[Int]] = []
-var idx = 0
+let caseCount = Int(readLine()!)!
 let dx = [1,0,-1,0]
 let dy = [0,1,0,-1]
-var minDay = 0
-var check = false
-for _ in 0..<input[0] {
-    maze.append(readLine()!.map { $0 })
-}
-//초기세팅
-for i in 0..<input[0] {
-   for j in 0..<input[1] {
-       if maze[i][j] == "F" {
-           fire[i][j] = 0
-           fireQueue.append([i,j])
-       }
-       if maze[i][j] == "J" {
-           person[i][j] = 0
-           personQueue.append([i,j])
-       }
-   }
-}
-//불이 어느 시간에 도착하는지 먼저 구하기
-while idx < fireQueue.count {
-    let current = fireQueue[idx]
-    idx += 1
-    for i in 0..<dx.count {
-        let newX = current[0] + dx[i]
-        let newY = current[1] + dy[i]
-        if newX < 0 || newY < 0 || newX >= input[0] || newY >= input[1] {
-            continue
+for _ in 0..<caseCount {
+    let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+    var building: [[String.Element]] = []
+    var fire = Array(repeating: Array(repeating: -1, count: input[0]), count: input[1])
+    var person = Array(repeating: Array(repeating: -1, count: input[0]), count: input[1])
+    var fireQueue: [[Int]] = []
+    var personQueue: [[Int]] = []
+    var minTime = 0
+    var check = false
+    var idx = 0
+    for _ in 0..<input[1] {
+        building.append(readLine()!.map { $0 })
+    }
+    //시작점 넣기
+    for i in 0..<input[1] {
+        for j in 0..<input[0] {
+            if building[i][j] == "*" {
+                fireQueue.append([i,j])
+                fire[i][j] = 0
+            } else if building[i][j] == "@" {
+                personQueue.append([i,j])
+                person[i][j] = 0
+            }
         }
-        if fire[newX][newY] == -1 && maze[newX][newY] != "#" {
-            fire[newX][newY] = fire[current[0]][current[1]] + 1
+    }
+    //불
+    while idx < fireQueue.count {
+        let current = fireQueue[idx]
+        idx += 1
+        for k in 0..<dx.count {
+            let newX = current[0] + dx[k]
+            let newY = current[1] + dy[k]
+            if newX < 0 || newY < 0 || newX >= input[1] || newY >= input[0] {
+                continue
+            }
+            if fire[newX][newY] != -1 || building[newX][newY] == "#" {
+                continue
+            }
             fireQueue.append([newX,newY])
+            fire[newX][newY] = fire[current[0]][current[1]] + 1
         }
     }
-}
-idx = 0
-//불보다 먼저 도착하는 경우만 생각
-outer: while idx < personQueue.count {
-    let current = personQueue[idx]
-    idx += 1
-    for i in 0..<dx.count {
-        let newX = current[0] + dx[i]
-        let newY = current[1] + dy[i]
-        if newX < 0 || newY < 0 || newX >= input[0] || newY >= input[1] {
-            //경계를 벗어난 것이므로 탈출한것임.
-            check = true
-            minDay = person[current[0]][current[1]] + 1
-            break outer
+    idx = 0
+    //사람
+    outer: while idx < personQueue.count {
+        let current = personQueue[idx]
+        idx += 1
+        for k in 0..<dx.count {
+            let newX = current[0] + dx[k]
+            let newY = current[1] + dy[k]
+            if newX < 0 || newY < 0 || newX >= input[1] || newY >= input[0] {
+                minTime = person[current[0]][current[1]] + 1
+                check = true
+                break outer
+            }
+            if person[newX][newY] >= 0 || building[newX][newY] == "#" {
+                continue
+            }
+            if fire[newX][newY] != -1 && fire[newX][newY] <= person[current[0]][current[1]] + 1 {
+                continue
+            }
+            personQueue.append([newX,newY])
+            person[newX][newY] = person[current[0]][current[1]] + 1
         }
-        if person[newX][newY] >= 0 || maze[newX][newY] == "#" {
-            continue
-        }
-        if fire[newX][newY] != -1 && fire[newX][newY] <= person[current[0]][current[1]] + 1 {
-            continue
-        }
-        person[newX][newY] = person[current[0]][current[1]] + 1
-        personQueue.append([newX, newY])
     }
-}
-if check {
-    print(minDay)
-} else {
-    print("IMPOSSIBLE")
+    if check {
+        print(minTime)
+    } else {
+        print("IMPOSSIBLE")
+    }
 }
