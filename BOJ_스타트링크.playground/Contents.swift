2db3bd1d251a75b2174cbe36feb5b5d032f7b275
hee <kkll135@naver.com>
2023-01-15 19:52:57 +0900

    코드수정

--- BOJ_스타트링크.playground/Contents.swift
+++ BOJ_스타트링크.playground/Contents.swift
@@ -1,60 +1,11 @@
 import Foundation
 
-final class FileIO {
-    private var buffer:[UInt8]
-    private var index: Int
-    
-    init(fileHandle: FileHandle = FileHandle.standardInput) {
-        buffer = Array(fileHandle.readDataToEndOfFile())+[UInt8(0)]
-        index = 0
-    }
-    
-    @inline(__always) private func read() -> UInt8 {
-        defer { index += 1 }
-        
-        return buffer.withUnsafeBufferPointer { $0[index] }
-    }
-    
-    @inline(__always) func readInt() -> Int {
-        var sum = 0
-        var now = read()
-        var isPositive = true
-        
-        while now == 10
-                || now == 32 { now = read() }
-        if now == 45{ isPositive.toggle(); now = read() }
-        while now >= 48, now <= 57 {
-            sum = sum * 10 + Int(now-48)
-            now = read()
-        }
-        
-        return sum * (isPositive ? 1:-1)
-    }
-    
-    @inline(__always) func readString() -> String {
-        var str = ""
-        var now = read()
-        
-        while now == 10
-                || now == 32 { now = read() }
-        
-        while now != 10
-                && now != 32 && now != 0 {
-            str += String(bytes: [now], encoding: .ascii)!
-            now = read()
-        }
-        
-        return str
-    }
-}
-
-let file = FileIO()
-
-let f = file.readInt()
-let s = file.readInt()
-let g = file.readInt()
-let u = file.readInt()
-let d = file.readInt()
+let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+let f = input[0]
+let s = input[1]
+let g = input[2]
+let u = input[3]
+let d = input[4]
 
 let dy = [u,-d]
 var floor = Array(repeating: -1, count: f + 1)
@@ -79,8 +30,4 @@ while !queue.isEmpty {
         }
     }
 }
-if floor[g] == -1 {
-    print("use the stairs")
-} else {
-    print(floor[g])
-}
+floor[g] == -1 ? print("use the stairs") : print(floor[g])
