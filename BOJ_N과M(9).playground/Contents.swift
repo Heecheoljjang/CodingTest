984ec0bde7d3e557ec6400f63e3b67ce81da49d3
hee <kkll135@naver.com>
2023-01-17 23:47:35 +0900

    PS

---
+++ BOJ_N과M(9).playground/Contents.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+//let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+//let numbers = readLine()!.split(separator: " ").map { Int(String($0))! }.sorted(by: <)
+let input = [4,2]
+let numbers = [9,7,9,1].sorted(by: <)
+var check = [Bool](repeating: false, count: input[0])
+var result: [[Int]] = []
+func dfs(_ arr: [Int]) {
+    var last = 0
+    if arr.count == input[1] {
+        result.append(arr)
+        return
+    }
+    for i in 0..<input[0] {
+        if !check[i] && last != numbers[i] {
+            check[i] = true
+            dfs(arr + [numbers[i]])
+            last = numbers[i]
+            check[i] = false
+        }
+    }
+}
+dfs([])
+for num in result {
+    print(num.map { String($0) }.joined(separator: " "))
+}
