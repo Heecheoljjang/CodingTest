b4f3f2af80082d1bb682fca948e63657d237ebff
hee <kkll135@naver.com>
2023-01-30 00:52:19 +0900

    PS

--- BOJ_01타일.playground/Contents.swift
+++ BOJ_01타일.playground/Contents.swift
@@ -1,3 +1,15 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+//let n = Int(readLine()!)!
+let n = 2
+if n <= 2 {
+    print(n)
+    exit(0)
+}
+var result = Array(repeating: 0, count: n + 1)
+result[1] = 1
+result[2] = 2
+for i in 3...n {
+    result[i] = (result[i - 1] + result[i - 2]) % 15746
+}
+print(result[n])
