e0e39e6c07c5ff6d82597a8788fd9f4c9214c5f0
hee <kkll135@naver.com>
2023-02-12 14:49:13 +0900

    PS

---
+++ BOJ_별찍기10.playground/Contents.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+func printStar(_ number: Int, _ pattern: [String]) {
+    if number == 1 {
+        pattern.forEach {
+            print($0)
+        }
+        return
+    }
+    let first = pattern.map { $0 + $0 + $0 }
+    let second = pattern.map { $0 + String(repeating: " ", count: $0.count) + $0 }
+    let new = first + second + first
+    printStar(number / 3, new)
+}
+//let n = Int(readLine()!)!
+let n = 27
+printStar(n, ["*"])
