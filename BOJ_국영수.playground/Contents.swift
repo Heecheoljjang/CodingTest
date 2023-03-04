9327ed39070af47390f38c3854798e11152f74fd
Heecheoljjang <77842318+Heecheoljjang@users.noreply.github.com>
2023-03-04 11:26:49 +0900

    PS

---
+++ BOJ_국영수.playground/Contents.swift
@@ -0,0 +1,73 @@
+import Foundation
+
+final class FileIO {
+    private var buffer:[UInt8]
+    private var index: Int
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(fileHandle.readDataToEndOfFile())+[UInt8(0)]
+        index = 0
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer.withUnsafeBufferPointer { $0[index] }
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() }
+        if now == 45{ isPositive.toggle(); now = read() }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var str = ""
+        var now = read()
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        
+        while now != 10
+                && now != 32 && now != 0 {
+            str += String(bytes: [now], encoding: .ascii)!
+            now = read()
+        }
+        
+        return str
+    }
+}
+
+let file = FileIO()
+let n = file.readInt()
+var students: [(String,Int,Int,Int)] = []
+for _ in 0..<n {
+    students.append((file.readString(),file.readInt(),file.readInt(),file.readInt()))
+}
+students.sort {
+    if $0.1 == $1.1 {
+        if $0.2 == $1.2 {
+            if $0.3 == $1.3 {
+                return $0.0 < $1.0
+            }
+            return $0.3 > $1.3
+        }
+        return $0.2 < $1.2
+    }
+    return $0.1 > $1.1
+}
+var answer = ""
+for student in students {
+    answer += "\(student.0)\n"
+}
+print(answer)
