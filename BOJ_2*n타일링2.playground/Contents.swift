e5c45af0804d135cc0ccdb2a0f5a212bf88cfcb4
Heecheoljjang <77842318+Heecheoljjang@users.noreply.github.com>
2023-03-08 23:04:16 +0900

    수정

--- BOJ_2*n타일링2.playground/Contents.swift
+++ BOJ_2*n타일링2.playground/Contents.swift
@@ -1,13 +1,13 @@
 import Foundation
 
 let n = Int(readLine()!)!
-var result = Array(repeating: 0, count: 1001)
+var result = Array(repeating: 0, count: n + 1)
 result[1] = 1; result[2] = 3
 if n <= 2 {
     print(result[n])
     exit(0)
 }
 for i in 3...n {
-    result[i] = result[i - 1] + 2 * result[i - 2]
+    result[i] = (result[i - 1] + 2 * result[i - 2]) % 10007
 }
 print(result[n])
